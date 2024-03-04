49c8534849b2aaa22e47ffbaf0fff3d3e79a2fc9
Brandon Kramer <brandonleekramer@gmail.com>
2024-03-03 19:04:45 -0500

    Updating explorerBalance and explorerBalances to work across chains

---
+++ R/explorerBalance.R
@@ -0,0 +1,42 @@
+#' Get the balance of the specified address in ETH
+#'
+#' @param wallet_address An Ethereum wallet balance (ENS domains not supported).
+#' @param blockchain_explorer A blockchain explorer API provider. Supported 
+#' @param api_key The API key of the Blockchain Explorer such as Etherscan or Arbiscan.
+#'
+#' @return Returns a dataframe with columns of account address, account balance, and currency.
+#' @export
+#'
+#' @examples
+#'
+#' explorerBalance(wallet_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+#'                 blockchain_explorer = "ethereum",     
+#'                 api_key = "A79S4QJXS518CS6ZQYFHB5SX5AWE7E952D")
+#'
+explorerBalance = function(wallet_address, 
+                           blockchain_explorer, 
+                           api_key){
+  
+  if (blockchain_explorer == "ethereum"){ explorer_url = "api.etherscan.io" } 
+  if (blockchain_explorer == "arbitrum"){ explorer_url = "api.arbiscan.io" }
+  if (blockchain_explorer == "base"){ explorer_url = "api.basescan.org" }
+  if (blockchain_explorer == "polygon"){ explorer_url = "api.polygonscan.com" } 
+  if (blockchain_explorer == "optimism"){ explorer_url = "api-optimistic.etherscan.io" } 
+  if (blockchain_explorer == "scroll"){ explorer_url = "api.scrollscan.com" } 
+  #else if (print("This blockchain explorer is not yet supported by onchainR."))
+  
+  url = stringr::str_c("https://",explorer_url,"/api",
+                       "?module=account",
+                       "&action=balance",
+                       "&address=",wallet_address,
+                       "&tag=latest",
+                       "&apikey=",api_key)
+  url_result <- jsonlite::fromJSON(url)
+  url_result = url_result$result 
+  url_result = data.frame(
+    account = wallet_address,
+    balance = as.numeric(url_result)/(10^18),
+    currency = "ETH"
+  )
+  url_result
+}
---
+++ R/explorerBalances.R
@@ -0,0 +1,44 @@
+#' Get the balance of multiple addresses in ETH
+#'
+#' @param wallet_addresses An Ethereum wallet balance (ENS domains not supported).
+#' @param blockchain_explorer A blockchain explorer API provider: "etherscan", "arbiscan".
+#' @param api_key The API key of the Blockchain Explorer such as Etherscan or Arbiscan.
+#'
+#' @return Returns a dataframe with columns of account addresses, account balances, and currencies.
+#' @export
+#'
+#' @examples
+#' blockchain_wallets = c("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+#'                        "0x50EC05ADe8280758E2077fcBC08D878D4aef79C3")
+#'
+#' explorerBalances(wallet_addresses = blockchain_wallets,
+#'                  blockchain_explorer = "ethereum",     
+#'                  api_key = "A79S4QJXS518CS6ZQYFHB5SX5AWE7E952D")
+#'
+explorerBalances = function(wallet_addresses, 
+                            blockchain_explorer, 
+                            api_key){
+  
+  if (blockchain_explorer == "ethereum"){ explorer_url = "api.etherscan.io" } 
+  if (blockchain_explorer == "arbitrum"){ explorer_url = "api.arbiscan.io" }
+  if (blockchain_explorer == "base"){ explorer_url = "api.basescan.org" }
+  if (blockchain_explorer == "polygon"){ explorer_url = "api.polygonscan.com" } 
+  if (blockchain_explorer == "optimism"){ explorer_url = "api-optimistic.etherscan.io" } 
+  if (blockchain_explorer == "scroll"){ explorer_url = "api.scrollscan.com" } 
+  #else if (print("This blockchain explorer is not yet supported by onchainR."))
+  
+  wallet_addresses = paste0(wallet_addresses, collapse = ",")
+  
+  url = stringr::str_c("https://",explorer_url,"/api",
+                       "?module=account",
+                       "&action=balancemulti",
+                       "&address=",wallet_addresses,
+                       "&tag=latest",
+                       "&apikey=",api_key)
+  url_result <- jsonlite::fromJSON(url)
+  url_result = url_result$result 
+  url_result = as.data.frame(url_result)
+  url_result$balance = as.numeric(url_result$balance)/(10^18)
+  url_result$currency = "ETH"
+  url_result
+}
