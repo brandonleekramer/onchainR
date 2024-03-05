9d8819d5a05399fa91f104ad0e8d8eb6f72f8208
Brandon Kramer <brandonleekramer@gmail.com>
2024-03-04 22:45:08 -0500

    Updating query_subgraph

--- R/query_subgraph.R
+++ R/query_subgraph.R
@@ -3,7 +3,6 @@
 #' @param graphql_query A GraphQL query of a subgraph.
 #' @param subgraph_id Subgraph ID (found near top of subgraph page on Graph Explorer)
 #' @param api_key An API key from Subgraph Studio.
-#' @param deployment_network Network that subgraph was deployed/published on: "arbitrum" or "ethereum"
 #'
 #' @return Returns subgraph data from an indexed smart contract.
 #' @export
@@ -11,28 +10,28 @@
 #' @examples
 #' uniswap_query = "{factories(first: 5){ id poolCount txCount totalVolumeUSD }}"
 #' 
-#' query_subgraph(uniswap_query, 
+#' query_subgraph(graphql_query = uniswap_query, 
 #'                subgraph_id = "5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV", 
-#'                api_key = "3a2f2d6ce1672801c3a044139373a287", 
-#'                deployment_network = "arbitrum")
+#'                api_key = "3a2f2d6ce1672801c3a044139373a287")
 #' 
 query_subgraph = function(graphql_query, 
                           subgraph_id, 
-                          api_key, 
-                          deployment_network){
+                          api_key#, 
+                          #deployment_network
+                          ){
   
-  if (deployment_network == "ethereum") {                       
-  graphql_conn <- ghql::GraphqlClient$new(
-    url = stringr::str_c("https://gateway.network.thegraph.com/api/subgraphs/id/", subgraph_id), 
-    headers = list(Authorization = paste0("Bearer ", api_key)))
-  } 
-  if (deployment_network == "arbitrum") {
+  #if (deployment_network == "ethereum") {                       
+  #graphql_conn <- ghql::GraphqlClient$new(
+  #  url = stringr::str_c("https://gateway.network.thegraph.com/api/subgraphs/id/", subgraph_id), 
+  #  headers = list(Authorization = paste0("Bearer ", api_key)))
+  #} 
+  #if (deployment_network == "arbitrum") {
     graphql_conn <- ghql::GraphqlClient$new(
       url = stringr::str_c("https://gateway-arbitrum.network.thegraph.com/api/subgraphs/id/", subgraph_id), 
       headers = list(Authorization = paste0("Bearer ", api_key)))
-  } else {
-    print("Please specify deployment_network as 'arbitrum' or 'ethereum'.")
-  }
+  #} else if {
+  #  print("Please specify deployment_network as 'arbitrum' or 'ethereum'.")
+  #}
   
   qry <- ghql::Query$new()
   qry$query('query_table', graphql_query)
